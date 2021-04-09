e99848011c8b400c604ede5dcab24dabb8612abc
Tasha Egbuna <tasha.egbuna@gmail.com>
2021-04-08 20:49:48 -0500

    add initial files for firebase authentication

---
+++ src/components/Header/Header.js
@@ -0,0 +1,21 @@
+import { login, logout } from '../../services/firebase';
+
+const Header = (props) => (
+    <header>
+       <a href="https://imgur.com/tU3hMkr"><img src="https://i.imgur.com/tU3hMkr.png" title="source: imgur.com" /></a>
+        <ul>
+            {
+                props.user ?
+                <>
+                    <li>Welcome, {props.user.displayName}</li>
+                    <li><img src={props.user.photoURL} alt={props.user.displayName} /></li>
+                    <li className="auth-link" onClick={logout}>Logout</li>
+                </>
+                :
+                <li className="auth-link" onClick={login}>Login</li>
+            }
+        </ul>
+    </header>
+); 
+
+export default Header;
---
+++ src/services/firebase.js
@@ -0,0 +1,38 @@
+//Import firebase core module
+import firebase from 'firebase/app';
+//import auth package from firebase
+import 'firebase/auth';
+
+const firebaseConfig = {
+  // Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyDjCPpxbggnjAalY8pQ75SHfj4mps-S_lY",
+    authDomain: "done-63ec6.firebaseapp.com",
+    projectId: "done-63ec6",
+    storageBucket: "done-63ec6.appspot.com",
+    messagingSenderId: "700237742754",
+    appId: "1:700237742754:web:de839348fd0bda86f74dfb"
+  };
+
+  //Initialize app
+firebase.initializeApp(firebaseConfig);
+
+//Set up  & configure firebase provider(s)
+const provider = new firebase.auth.GoogleAuthProvider();
+const auth = firebase.auth();
+
+//Setup auth actions (login, logout, etc...)
+function login() {
+    auth.signInWithPopup(provider);
+}
+
+function logout() {
+    auth.signOut();
+}
+
+//Export actions
+export {
+    auth,
+    login,
+    logout,
+}
